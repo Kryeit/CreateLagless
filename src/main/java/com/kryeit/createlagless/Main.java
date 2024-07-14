2f3721d19e37e3f2c6ad40e9f4f9c40a74e5ea38
muriplz <46825658+muriplz@users.noreply.github.com>
2024-07-14 22:22:23 +0200

    save List as static in RAM

--- src/main/java/com/kryeit/createlagless/Main.java
+++ src/main/java/com/kryeit/createlagless/Main.java
@@ -1,16 +1,28 @@
 package com.kryeit.createlagless;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.simibubi.create.content.decoration.palettes.AllPaletteStoneTypes;
+
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 
 public class Main implements ModInitializer {
     public static final String MOD_ID = "createlagless";
     public static final Logger LOGGER = LoggerFactory.getLogger("Create: Lagless");
 
-	@Override
-	public void onInitialize() {
+	public static final List<Item> collideBypass = List.of(
+			Items.COBBLESTONE,
+			Items.STONE,
+			Items.CALCITE,
+			AllPaletteStoneTypes.SCORCHIA.baseBlock.get().asItem(),
+			AllPaletteStoneTypes.SCORIA.baseBlock.get().asItem()
+	);
 
-	}
+	@Override
+	public void onInitialize() {}
 }
--- src/main/java/com/kryeit/createlagless/mixin/create/AbstractContraptionEntityMixin.java
+++ src/main/java/com/kryeit/createlagless/mixin/create/AbstractContraptionEntityMixin.java
@@ -1,23 +1,16 @@
 package com.kryeit.createlagless.mixin.create;
 
-import com.simibubi.create.content.contraptions.AbstractContraptionEntity;
-
-import com.simibubi.create.content.decoration.palettes.AllPaletteStoneTypes;
-
-import net.minecraft.entity.Entity;
-
-import net.minecraft.entity.ItemEntity;
-
-import net.minecraft.item.Item;
-
-import net.minecraft.item.Items;
+import static com.kryeit.createlagless.Main.collideBypass;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.List;
+import com.simibubi.create.content.contraptions.AbstractContraptionEntity;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
 
 @Mixin(AbstractContraptionEntity.class)
 public class AbstractContraptionEntityMixin {
@@ -31,14 +24,6 @@ public class AbstractContraptionEntityMixin {
 
 
 		if (e instanceof ItemEntity ie) {
-			List<Item> collideBypass = List.of(
-					Items.COBBLESTONE,
-					Items.STONE,
-					Items.CALCITE,
-					AllPaletteStoneTypes.SCORCHIA.baseBlock.get().asItem(),
-					AllPaletteStoneTypes.SCORIA.baseBlock.get().asItem()
-			);
-
 			if (collideBypass.contains(ie.getStack().getItem())) {
 				cir.setReturnValue(false);
 			}
